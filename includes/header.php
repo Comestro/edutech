a899310f60340239b1cf3395e7fff9f0718db31b
sadique-cws <sadique.hussain96@gmail.com>
2025-01-24 08:16:39 +0530

    adding signup page

---
+++ includes/header.php
@@ -0,0 +1,7 @@
+<div class="navbar navbar-expand-lg bg-light navbar-light">
+    <div class="container">
+        <a href="" class="navbar-brand">
+            <img src="assets/images/edutechlogo.png" alt="">
+        </a>
+    </div>
+</div>
---
+++ login.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/login.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body class="bg-light">
+    <div class="container wrapper d-flex align-items-center ">
+        <div class="row">
+            <div class="col-lg-7">
+                <img src="assets/images/logo_large.png" width="80%" alt="">
+                <p class="fw-medium h5">EduNet is Educational Network helps you connect and share with the Students & teacher.</p>
+            </div>
+            <div class="col-lg-5">
+                <div class="bg-white card">
+                    <div class="card-body">
+                        <form action="" method="post">
+                            <div class="row">
+                                <div class="mb-3 col">
+                                    <label for="" class="small text-muted">First Name</label>
+                                    <input type="text" name="firstname" placeholder="e.g Anil" class="form-control rounded-0">
+                                </div>
+                                <div class="mb-3 col">
+                                    <label for="" class="small text-muted">Last Name</label>
+                                    <input type="text" name="lastname" placeholder="e.g Kumar" class="form-control rounded-0">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="mb-3 col">
+                                    <label for="" class="small text-muted">Day</label>
+                                    <select name="day" id="" class="form-select rounded-0">
+                                        <option value="">Select Day</option>
+                                    </select>
+                                </div>
+                                <div class="mb-3 col">
+                                    <label for="" class="small text-muted">month</label>
+                                    <select name="month" id="" class="form-select rounded-0">
+                                        <option value="">Select Month</option>
+                                    </select>
+                                </div>
+                                <div class="mb-3 col">
+                                    <label for="" class="small text-muted">Year</label>
+                                    <select name="" id="" class="form-select rounded-0">
+                                        <option value="">Select year</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
